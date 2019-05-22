fc7dcbc9705ba7835a24c559247fe835b838ba18
Karyna <karina.labazova@gmail.com>
2019-05-22 16:28:41 +0300

    login

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import {FacebookShareButton,FacebookIcon,
         TelegramShareButton,TelegramIcon,
         EmailShareButton,EmailIcon
 } from 'react-share';
+import {Provider, connect}   from 'react-redux';
+import {createStore} from 'redux';
+import thunk from 'redux-thunk';
+const applyMiddleware = require("redux").applyMiddleware
 
 library.add(faPlus, faChevronRight, faTimes)
 const customStyles = {
@@ -26,10 +30,65 @@ const customStyles = {
   }
 };
 
+let store = createStore((state, action) => {
+  if(!state){
+    return {promises: {}}
+  }
+  if(action.type === 'PROMISE' && action.name){
+    return { promises: {...state.promises,
+                          [action.name]: {
+                            status: action.status,
+                            error: action.error,
+                            data: action.data
+                          }}}
+  }
+  return state;
+}, applyMiddleware(thunk))
+
+store.subscribe(()=> console.log(store.getState()))
+
+function makePromiseActions(name,promise){
+  const actionPending     = () => ({ name, type: 'PROMISE', status: 'PENDING', data: null, error: null, value:"" })
+  const actionResolved    = data => ({ name, type: 'PROMISE', status: 'RESOLVED', data, error: null, value: "" })
+  const actionRejected    = error => ({ name, type: 'PROMISE', status: 'REJECTED', data: null, error, value: "" })
+
+  function actionPromise(){
+    return async function (dispatch){
+      dispatch(actionPending())
+      try{
+        dispatch(actionResolved(await promise))
+      }
+      catch(e){
+        dispatch (actionRejected(e))
+      }
+    }
+  }
+  return actionPromise
+}
+let authToken
+
+let login = (nick,password) =>  ( 
+  fetch ('http://localhost:4000/login', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username: nick, password: password})
+  })
+)
+
 class LoginPage extends Component{
   constructor(props){
     super(props);
+    this.logIn = this.logIn.bind(this)
+    this.state = {nick: null, password: null}
   }
+
+  logIn (nick, password) {
+    store.dispatch(makePromiseActions('login',login(nick, password))())
+  }
+
   render(){
 
     const responseGoogle = (response) => {
@@ -41,9 +100,12 @@ class LoginPage extends Component{
             <form className="login">
               <h2>Welcome to <span>Wishful</span></h2>
               <p>please login to continue</p>
-              <input type="text" placeholder="username"/>
-              <input type="password" placeholder="password"/>
-              <input type="button" value="GO!"/>
+              <input type="text" placeholder="username"
+                onChange ={event => this.setState({nick: event.target.value})}/>
+              <input type="password" placeholder="password"
+                onChange ={event => this.setState({password: event.target.value})}/>
+              <input type="button" value="GO!"
+                onClick={()=>this.logIn(this.state.nick,this.state.password)}/>
             </form>
             <p> or </p>
             <div className="createAccount">
